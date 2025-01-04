aafd732288458b2f4ffb11c47e86c265ee29b22b
Thiago009 <thiago.kaique01@gmail.com>
2025-01-04 15:44:38 -0300

    Refactor

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -17,7 +17,22 @@ RE::TESObjectWEAP* GetEquipedStaff(RE::Actor* a_actor, RE::BGSEquipSlot** a_slot
     }
     return nullptr;
 }
+RE::ExtraDataList* GetInventoryExtraList(RE::Actor* actor, RE::TESBoundObject* object) {
+    auto inv = actor->GetInventory();
 
+    auto it = inv.find(object);
+
+    if (it != inv.end()) {
+        if (auto& data = it->second.second) {
+            if (!data->extraLists->empty()) {
+                if (auto extra = data->extraLists->front()) {
+                    return extra;
+                }
+            }
+        }
+    }
+    return nullptr;
+}
 void RefreshEquipedItem(RE::Actor* a_actor, RE::TESBoundObject* a_object, RE::ExtraDataList* a_extraData, RE::BGSEquipSlot* a_slot) {
 
     RE::ActorEquipManager::GetSingleton()->UnequipObject(a_actor, a_object, nullptr, 1, a_slot, false, false, false, true,nullptr);
@@ -30,61 +45,37 @@ void Hooks::EquipSpellHook::thunk(RE::ActorEquipManager* a_manager, RE::Actor* a
     if (auto staff = GetEquipedStaff(a_actor, a_slot)) {
         logger::trace("{} Attempted to replace staff staff {} equiped with spell {}", a_actor->GetName(), staff->GetName(), a_spell->GetName());
 
-        auto inv = a_actor->GetInventory();
+        if (auto extra = GetInventoryExtraList(a_actor, staff)) {
+            auto form = RE::TESForm::LookupByID<RE::EnchantmentItem>(0x29b5c);
 
-        auto it = inv.find(staff);
+            logger::trace("enchantment {}", form->GetFormType());
 
-        if (it != inv.end()) {
-            if (auto& data = it->second.second) {
-                if (!data->extraLists->empty()) {
-                    if (auto extra = data->extraLists->front()) {
-
-
-                        auto form = RE::TESForm::LookupByID<RE::EnchantmentItem>(0x29b5c);
-
-
-                        logger::trace("enchantment {}", form->GetFormType());
-
-
-                        if (extra->HasType<RE::ExtraEnchantment>()) {
-                            auto e = extra->GetByType<RE::ExtraEnchantment>();
-                            e->enchantment = form;
-                            e->charge = 10000;
-                        } else {
-                            auto enchantment_fake = RE::BSExtraData::Create<RE::ExtraEnchantment>();
-                            enchantment_fake->charge = 10000;
-                            enchantment_fake->enchantment = form;   
-                            extra->Add(enchantment_fake);
-
-                            if (auto actor = a_actor->AsActorValueOwner()) {
-                                bool wornLeft = extra->HasType(RE::ExtraDataType::kWornLeft);
-                                bool wornRight = !wornLeft && extra->HasType(RE::ExtraDataType::kWorn);
-                                if (wornLeft || wornRight) {
-                                    RE::ActorValue actorValue =
-                                        wornLeft ? RE::ActorValue::kLeftItemCharge : RE::ActorValue::kRightItemCharge;
-
-                                    float itemCharge = actor->GetActorValue(actorValue);
-
-                                    actor->ModActorValue(actorValue, 10000);
-
-                                    RefreshEquipedItem(a_actor, staff, extra, *a_slot);
-  
-                                }
-                            }
-                        }
-                    } else {
-                        logger::trace("no fonrt");
-                    }
-                } else {
-                    logger::trace("not in inventory");
-                }
+            if (extra->HasType<RE::ExtraEnchantment>()) {
+                auto e = extra->GetByType<RE::ExtraEnchantment>();
+                e->enchantment = form;
+                e->charge = 10000;
             } else {
-                logger::trace("no data");
-            }
-        } else {
-            logger::trace("not in inventory");
-        }
+                auto enchantment_fake = RE::BSExtraData::Create<RE::ExtraEnchantment>();
+                enchantment_fake->charge = 10000;
+                enchantment_fake->enchantment = form;
+                extra->Add(enchantment_fake);
 
+                if (auto actor = a_actor->AsActorValueOwner()) {
+                    bool wornLeft = extra->HasType(RE::ExtraDataType::kWornLeft);
+                    bool wornRight = !wornLeft && extra->HasType(RE::ExtraDataType::kWorn);
+                    if (wornLeft || wornRight) {
+                        RE::ActorValue actorValue =
+                            wornLeft ? RE::ActorValue::kLeftItemCharge : RE::ActorValue::kRightItemCharge;
+
+                        float itemCharge = actor->GetActorValue(actorValue);
+
+                        actor->ModActorValue(actorValue, 10000);
+
+                        RefreshEquipedItem(a_actor, staff, extra, *a_slot);
+                    }
+                }
+            }
+        }
         return;
     }
 
